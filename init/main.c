c7f5ee3d14620e4da3d77003687d879a228b3ca4
MSathieu <18145111+MSathieu@users.noreply.github.com>
2021-03-27 09:32:40 +0100

    init: Embed capabilities in service array

--- init/main.c
+++ init/main.c
@@ -8,30 +8,48 @@
 #include <sys/wait.h>
 #include <unistd.h>
 
-struct process {
+struct service {
   char* name;
   bool raw;
   pid_t pid;
+  size_t capabilities[64];
 };
 
-static struct process processes[] = {{"argd", 1, 0}, {"atad", 1, 0}, {"ipcd", 1, 0}, {"logd", 1, 0}, {"/sbin/envd", 0, 0}, {"/sbin/fbd", 0, 0}, {"/sbin/kbdd", 0, 0}, {"/sbin/ps2d", 0, 0}, {"/sbin/ttyd", 0, 0}, {"vfsd", 1, 0}};
+struct service services[] = {
+  {"argd", 1, 0, {[CAP_NAMESPACE_KERNEL] = 1 << CAP_KERNEL_PRIORITY}},
+  {"atad", 1, 0, {[CAP_NAMESPACE_KERNEL] = 1 << CAP_KERNEL_PRIORITY, [CAP_NAMESPACE_FILESYSTEMS] = 1 << CAP_VFSD_MOUNT}},
+  {"ipcd", 1, 0, {[CAP_NAMESPACE_KERNEL] = 1 << CAP_KERNEL_PRIORITY}},
+  {"logd", 1, 0, {[CAP_NAMESPACE_KERNEL] = 1 << CAP_KERNEL_PRIORITY, [CAP_NAMESPACE_SERVERS] = 1 << CAP_LOGD}},
+  {"/sbin/envd", 0, 0, {[CAP_NAMESPACE_KERNEL] = 1 << CAP_KERNEL_PRIORITY}},
+  {"/sbin/fbd", 0, 0, {[CAP_NAMESPACE_KERNEL] = 1 << CAP_KERNEL_PRIORITY | 1 << CAP_KERNEL_GET_FB_INFO}},
+  {"/sbin/kbdd", 0, 0, {[CAP_NAMESPACE_KERNEL] = 1 << CAP_KERNEL_PRIORITY, [CAP_NAMESPACE_SERVERS] = 1 << CAP_KBDD}},
+  {"/sbin/ps2d", 0, 0, {[CAP_NAMESPACE_KERNEL] = 1 << CAP_KERNEL_PRIORITY, [CAP_NAMESPACE_SERVERS] = 1 << CAP_KBDD_SEND_KEYPRESS}},
+  {"/sbin/ttyd", 0, 0, {[CAP_NAMESPACE_KERNEL] = 1 << CAP_KERNEL_PRIORITY, [CAP_NAMESPACE_DRIVERS] = 1 << CAP_FBD_DRAW, [CAP_NAMESPACE_SERVERS] = 1 << CAP_KBDD_RECEIVE_EVENTS | 1 << CAP_LOGD_TTY}},
+  {"vfsd", 1, 0, {[CAP_NAMESPACE_KERNEL] = 1 << CAP_KERNEL_PRIORITY, [CAP_NAMESPACE_FILESYSTEMS] = 1 << CAP_VFSD}},
+};
 
 static void spawn(const char* name) {
-  for (size_t i = 0; i < sizeof(processes) / sizeof(struct process); i++) {
-    if (!strcmp(processes[i].name, name)) {
-      if (processes[i].raw) {
-        processes[i].pid = spawn_process_raw(name);
+  bool service_found = 0;
+  for (size_t i = 0; i < sizeof(services) / sizeof(struct service); i++) {
+    if (!strcmp(services[i].name, name)) {
+      if (services[i].raw) {
+        services[i].pid = spawn_process_raw(name);
       } else {
-        processes[i].pid = spawn_process(name);
+        services[i].pid = spawn_process(name);
       }
+      for (size_t j = 0; j < 64; j++) {
+        _syscall(_SYSCALL_GRANT_CAPABILITIES, j, services[i].capabilities[j], 0, 0, 0);
+      }
+      service_found = 1;
       break;
     }
   }
+  if (!service_found) {
+    return;
+  }
   if (!strcmp(name, "argd")) {
     register_ipc_name("argd");
-    grant_capability(CAP_NAMESPACE_KERNEL, CAP_KERNEL_PRIORITY);
   } else if (!strcmp(name, "atad")) {
-    grant_capability(CAP_NAMESPACE_KERNEL, CAP_KERNEL_PRIORITY);
     for (size_t i = 0; i < 8; i++) {
       grant_ioport(0x1f0 + i);
       grant_ioport(0x170 + i);
@@ -40,44 +58,26 @@ static void spawn(const char* name) {
     grant_ioport(0x376);
     register_irq(14);
     register_irq(15);
-    grant_capability(CAP_NAMESPACE_FILESYSTEMS, CAP_VFSD_MOUNT);
-  } else if (!strcmp(name, "ipcd")) {
-    grant_capability(CAP_NAMESPACE_KERNEL, CAP_KERNEL_PRIORITY);
   } else if (!strcmp(name, "logd")) {
     register_ipc_name("logd");
-    grant_capability(CAP_NAMESPACE_KERNEL, CAP_KERNEL_PRIORITY);
-    grant_capability(CAP_NAMESPACE_SERVERS, CAP_LOGD);
   } else if (!strcmp(name, "/sbin/envd")) {
     register_ipc_name("envd");
-    grant_capability(CAP_NAMESPACE_KERNEL, CAP_KERNEL_PRIORITY);
   } else if (!strcmp(name, "/sbin/fbd")) {
     register_ipc_name("fbd");
-    grant_capability(CAP_NAMESPACE_KERNEL, CAP_KERNEL_GET_FB_INFO);
-    grant_capability(CAP_NAMESPACE_KERNEL, CAP_KERNEL_PRIORITY);
     uintptr_t fb_phys_addr = _syscall(_SYSCALL_GET_FB_INFO, 0, 0, 0, 0, 0);
     size_t height = _syscall(_SYSCALL_GET_FB_INFO, 2, 0, 0, 0, 0);
     size_t pitch = _syscall(_SYSCALL_GET_FB_INFO, 3, 0, 0, 0, 0);
     map_physical_memory(fb_phys_addr, height * pitch);
   } else if (!strcmp(name, "/sbin/kbdd")) {
     register_ipc_name("kbdd");
-    grant_capability(CAP_NAMESPACE_KERNEL, CAP_KERNEL_PRIORITY);
-    grant_capability(CAP_NAMESPACE_SERVERS, CAP_KBDD);
   } else if (!strcmp(name, "/sbin/ps2d")) {
-    grant_capability(CAP_NAMESPACE_KERNEL, CAP_KERNEL_PRIORITY);
-    grant_capability(CAP_NAMESPACE_SERVERS, CAP_KBDD_SEND_KEYPRESS);
     grant_ioport(0x60);
     grant_ioport(0x64);
     register_irq(1);
   } else if (!strcmp(name, "/sbin/ttyd")) {
     register_ipc_name("ttyd");
-    grant_capability(CAP_NAMESPACE_KERNEL, CAP_KERNEL_PRIORITY);
-    grant_capability(CAP_NAMESPACE_DRIVERS, CAP_FBD_DRAW);
-    grant_capability(CAP_NAMESPACE_SERVERS, CAP_KBDD_RECEIVE_EVENTS);
-    grant_capability(CAP_NAMESPACE_SERVERS, CAP_LOGD_TTY);
   } else if (!strcmp(name, "vfsd")) {
     register_ipc_name("vfsd");
-    grant_capability(CAP_NAMESPACE_KERNEL, CAP_KERNEL_PRIORITY);
-    grant_capability(CAP_NAMESPACE_FILESYSTEMS, CAP_VFSD);
   }
   start_process();
 }
@@ -97,12 +97,12 @@ int main(void) {
   spawn("/sbin/ps2d");
   while (1) {
     pid_t pid = wait(0);
-    for (size_t i = 0; i < sizeof(processes) / sizeof(struct process); i++) {
-      if (processes[i].pid == pid) {
-        if (!strcmp(processes[i].name, "ipcd")) {
+    for (size_t i = 0; i < sizeof(services) / sizeof(struct service); i++) {
+      if (services[i].pid == pid) {
+        if (!strcmp(services[i].name, "ipcd")) {
           return 1;
         }
-        spawn(processes[i].name);
+        spawn(services[i].name);
         break;
       }
     }
