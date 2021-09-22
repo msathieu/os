f9089cae2f1a271dedeead3a9d2d3db12a5b3b7b
msathieu <18145111+msathieu@users.noreply.github.com>
2021-09-22 08:51:30 +0200

    Handle power button

--- acpid/main.c
+++ acpid/main.c
@@ -1,9 +1,14 @@
 #include <__/syscall.h>
 #include <capability.h>
 #include <ipccalls.h>
+#include <irq.h>
 #include <lai/helpers/pm.h>
 #include <lai/helpers/sci.h>
+#include <pthread.h>
 #include <syslog.h>
+
+//TODO: Replace with mutex
+static pthread_spinlock_t lock;
 
 static int64_t power_state_handler(uint64_t type, uint64_t arg1, uint64_t arg2, uint64_t arg3, uint64_t arg4) {
   if (arg1 || arg2 || arg3 || arg4) {
@@ -16,11 +21,13 @@ static int64_t power_state_handler(uint64_t type, uint64_t arg1, uint64_t arg2, 
   }
   switch (type) {
   case 0:
+    pthread_spin_lock(&lock);
     lai_enter_sleep(5);
     while (1) {
       asm volatile("pause");
     }
-  case 1:;
+  case 1:
+    pthread_spin_lock(&lock);
     lai_api_error_t error = lai_acpi_reset();
     if (error) {
       _syscall(_SYSCALL_RESET, 0, 0, 0, 0, 0);
@@ -33,6 +40,14 @@ static int64_t power_state_handler(uint64_t type, uint64_t arg1, uint64_t arg2, 
     return -IPC_ERR_INVALID_ARGUMENTS;
   }
 }
+static int sci_handler(__attribute__((unused)) void* arg) {
+  wait_irq();
+  pthread_spin_lock(&lock);
+  lai_enter_sleep(5);
+  while (1) {
+    asm volatile("pause");
+  }
+}
 int main(void) {
   drop_capability(CAP_NAMESPACE_KERNEL, CAP_KERNEL_PRIORITY);
   register_ipc(0);
@@ -40,6 +55,9 @@ int main(void) {
   lai_set_acpi_revision(revision);
   lai_create_namespace();
   lai_enable_acpi(1);
+  lai_set_sci_event(ACPI_POWER_BUTTON);
+  pthread_spin_init(&lock, 0);
+  thrd_create(0, sci_handler, 0);
   ipc_handlers[IPC_ACPID_POWER_STATE] = power_state_handler;
   while (1) {
     handle_ipc();
